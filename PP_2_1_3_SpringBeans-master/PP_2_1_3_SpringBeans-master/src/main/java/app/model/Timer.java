e0edb001eb1a6df832a48d95959274a5312f98a8
miirchv <98304456+miirchv@users.noreply.github.com>
2022-07-27 22:07:20 +0400

    Add files via upload

---
+++ PP_2_1_3_SpringBeans-master/PP_2_1_3_SpringBeans-master/src/main/java/app/model/Timer.java
@@ -0,0 +1,16 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("animalsCage")
+public class Timer {
+
+    private Long nanoTime = System.nanoTime();
+
+    public Long getTime() {
+        return nanoTime;
+    }
+}
